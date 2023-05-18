81f1de57f8477ee6504343561771cc4c03459729
Juma <juma.musa@student.moringaschool.com>
2023-05-18 12:59:32 +0300

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
-# Make your shoe class here!
+ # Make your shoe class here!
+ 
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+  
+    def initialize(brand)
+      @brand = brand
+      @color = nil
+      @size = nil
+      @material = nil
+      @condition = "new"
+    end
+  
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+  end
+  
+  RSpec.describe Shoe do
+    describe "#cobble" do
+      it "says that the shoe has been repaired" do
+        shoe = Shoe.new("Nike")
+        expect { shoe.cobble }.to output("Your shoe is as good as new!\n").to_stdout
+      end
+    end
+  end
+  
